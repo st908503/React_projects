367f2021de04c7bd2a78f84abbf52c8ed127a305
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-01-22 20:11:23 +0530

    Add files via upload

---
+++ tinder-clone-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React   from 'react';
+import './App.css';
+import Header from "./Header"
+import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons'
+
+function App() {
+  return ( 
+    <div>
+      <Header/>
+      <TinderCards/>
+      <SwipeButtons/>
+
+    </div>
+  );
+}
+
+export default App;
